6bb6b439f2b7baaaf2acba5f156c2b4991d49952
Adam K Dean <adam@amberfish.co.uk>
2014-02-24 21:45:23 +0000

    Reintroduced labels...yay

---
+++ src/app/dom/labels.js
@@ -0,0 +1,14 @@
+var Labels = {
+	setLabel: function(label, text) {
+		var label = $('#' + label);
+		if (label) {
+			label.text(text);
+		}
+	},
+	hideLabel: function(label) {
+		$('#' + label).hide();
+	},
+	showLabel: function(label) {
+		$('#' + label).show();
+	},
+};
--- src/app/main.js
+++ src/app/main.js
@@ -1,11 +1,13 @@
 $(function() {
+
+	Labels.setLabel('tl', 'Darwin Project');
+	Labels.setLabel('tr', 'v0.0.2');
+	Labels.hideLabel('bl');
+	Labels.hideLabel('br');
+
 	var displayEngine = new DisplayEngine('canvas');
-
 	var grid = new Grid();
+
 	displayEngine.add(grid);
-
-
-
-	// and we're off
 	displayEngine.init();
 });
