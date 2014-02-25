3ce06eca0d058fb3c3a1f5ccc59a1065a81629d4
Adam Dean <adam.dean@bravissimo.com>
2014-02-25 08:21:00 +0000

    Merged ClientSize into DisplayEngine

--- src/app/display/DisplayEngine.js
+++ src/app/display/DisplayEngine.js
@@ -1,11 +1,49 @@
 function DisplayEngine(canvasId) {
 	this.canvas = document.getElementById(canvasId);
 	this.context = this.canvas.getContext('2d');
-	this.clientSize = new ClientSize();
 	this.canvasWidth = 0;
 	this.canvasHeight = 0;
 	this.elements = [];
 }
+
+DisplayEngine.prototype.getClientWidth = function() {
+	return this.filterResults (
+		window.innerWidth ? window.innerWidth : 0,
+		document.documentElement ? document.documentElement.clientWidth : 0,
+		document.body ? document.body.clientWidth : 0
+	);
+};
+
+DisplayEngine.prototype.getClientHeight = function() {
+	return this.filterResults (
+		window.innerHeight ? window.innerHeight : 0,
+		document.documentElement ? document.documentElement.clientHeight : 0,
+		document.body ? document.body.clientHeight : 0
+	);
+};
+
+DisplayEngine.prototype.getScrollLeft = function() {
+	return this.filterResults (
+		window.pageXOffset ? window.pageXOffset : 0,
+		document.documentElement ? document.documentElement.scrollLeft : 0,
+		document.body ? document.body.scrollLeft : 0
+	);
+};
+
+DisplayEngine.prototype.getScrollTop = function() {
+	return this.filterResults (
+		window.pageYOffset ? window.pageYOffset : 0,
+		document.documentElement ? document.documentElement.scrollTop : 0,
+		document.body ? document.body.scrollTop : 0
+	);
+};
+
+DisplayEngine.prototype.filterResults = function(n_win, n_docel, n_body) {
+	var n_result = n_win ? n_win : 0;
+	if (n_docel && (!n_result || (n_result > n_docel)))
+		n_result = n_docel;
+	return n_body && (!n_result || (n_result > n_body)) ? n_body : n_result;
+};
 
 DisplayEngine.prototype.requestAnimFrame = (function () {
     return window.requestAnimationFrame ||
@@ -22,8 +60,8 @@ DisplayEngine.prototype.add = function(element) {
 }
 
 DisplayEngine.prototype.resize = function () {
-	this.canvasWidth = this.clientSize.f_clientWidth();
-	this.canvasHeight = this.clientSize.f_clientHeight();
+	this.canvasWidth = this.getClientWidth();
+	this.canvasHeight = this.getClientHeight();
 	this.canvas.setAttribute('width', this.canvasWidth);
 	this.canvas.setAttribute('height', this.canvasHeight);
 	this.update();
