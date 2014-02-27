16f5a67c9b7f8a25618ec3e5f2c30cf0db19c0cf
Adam K Dean <adam@amberfish.co.uk>
2014-02-27 17:50:09 +0000

    Some test changes for paper.js

--- src/app/main.js
+++ src/app/main.js
@@ -1,11 +1,25 @@
 $(function() {
 
-	Labels.setLabel('tl', 'Darwin Project');
-	Labels.setLabel('tr', 'v0.0.2');
+    Labels.setLabel('tl', 'Darwin Project');
+    Labels.setLabel('tr', 'v0.0.3');
 
-	var displayEngine = new DisplayEngine('canvas');
-	var simulator = new Simulator();
-	displayEngine.add(simulator);
-    displayEngine.init();
+    // var displayEngine = new DisplayEngine('canvas');
+    // var simulator = new Simulator();
+    // displayEngine.add(simulator);
+    // displayEngine.init();
+
+    paper.setup(this.canvas);
+    // Create a Paper.js Path to draw a line into it:
+    var path = new paper.Path();
+    // Give the stroke a color
+    path.strokeColor = 'black';
+    var start = new paper.Point(100, 100);
+    // Move to start and draw a line from there
+    path.moveTo(start);
+    // Note that the plus operator on Point objects does not work
+    // in JavaScript. Instead, we need to call the add() function:
+    path.lineTo(start.add([ 200, -50 ]));
+    // Draw the view now:
+    paper.view.draw();
 
 });
